2486bbf6c7f16913052faa6e25943f65a92e4876
Ahmed Gado <a.gado.2df@gmail.com>
2019-07-25 00:06:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+// Write your solution in this file!
+var customerName ;
+function upperCaseCustomerName (){
+    customerName = 'CUSTOMERNAME'
+}
+function bestCustomer (){
+    bestCustomer = 'not bob' ;
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'Ahmed'
+}
+const leastFavoriteCustomer = 'Ahmed' ;
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Mido'
+    console.log(leastFavoriteCustomer);
+}
